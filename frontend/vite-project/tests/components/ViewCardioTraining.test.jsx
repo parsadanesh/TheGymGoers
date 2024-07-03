0c9cba23f6d796f213e1f3c7c1cb98d3992723e0
pad31 <pad31@aber.ac.uk>
2024-07-03 12:14:50 +0100

    NEW: refactored 1 test

--- frontend/vite-project/tests/components/ViewCardioTraining.test.jsx
+++ frontend/vite-project/tests/components/ViewCardioTraining.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 // import userEvent from "@testing-library/user-event";
-import ViewCardioTraining from "../../src/components/viewCardioTraining.jsx";
+import ViewCardioTraining from "../../src/components/ViewCardioTraining.jsx";
 
 import testData from "../sampleUsers.js";
 import ViewWorkout from "../../src/pages/ViewWorkouts.jsx";
